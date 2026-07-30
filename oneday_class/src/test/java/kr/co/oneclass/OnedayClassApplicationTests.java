e612de3cdb2137375ebf655f7f6e86c06f932799
user <user@DESKTOP-ATOVTEP>
2026-07-30 16:02:54 +0900

    프로젝트 구조정의

---
+++ oneday_class/src/test/java/kr/co/oneclass/OnedayClassApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.oneclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnedayClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
