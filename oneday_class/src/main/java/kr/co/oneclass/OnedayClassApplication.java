c27d0309d8248d48be6b1d2d85df355cdcf08619
user <user@DESKTOP-ATOVTEP>
2026-08-02 23:33:46 +0900

    프로젝트 구조 정의

---
+++ oneday_class/src/main/java/kr/co/oneclass/OnedayClassApplication.java
@@ -0,0 +1,13 @@
+package kr.co.oneclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedayClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnedayClassApplication.class, args);
+	}
+
+}
